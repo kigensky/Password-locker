b3e404439d51f35c9d0f6639c3a5f4ae0dedf5f8
victor kigen <vickigen@gmail.com>
2021-04-10 16:09:25 +0300

    add fuction that checks if the user exist

--- run.py
+++ run.py
@@ -27,3 +27,11 @@ def display_user():
     Function to display existing user
     """
     return User.display_user()    
+
+def login_user(username,password):
+    """
+    function that checks whether a user exist and then login the user in.
+    """
+  
+    check_user = Credentials.verify_user(username,password)
+    return check_user
